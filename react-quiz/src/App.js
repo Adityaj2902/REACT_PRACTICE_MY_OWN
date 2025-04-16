f168e42330f0cbf02a82d58280a426f6e25c691f
Adityaj2902 <adityajofficial909@gmail.com>
2025-04-16 15:12:56 +0530

    [Aditya Jaiswal] Add Create a FakeApi with the help of npm i json-server

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,8 +1,48 @@
-import DataCounter from './DateCounter'
+import { useEffect, useReducer } from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+
+const intialState = {
+  questions: [],
+
+
+  //'loading','error,'ready','active','finished'
+
+  status: "loading",
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return {
+        ...state,
+        questions: action.payload,
+        status:"ready"
+      }
+      case 'dataFailed':
+        return {...state,status:"error"}
+    default: throw new Error('Action Unknown ')
+  }
+}
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, intialState)
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ ...state, type: 'dataRecieved', payload: data }))
+      .catch((err) => dispatch({...state,type:'dataFailed',payload:err}))
+  })
+
   return (
-    <div>
-      <DataCounter/>
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question ?</p>
+      </Main>
     </div>
   );
 }
